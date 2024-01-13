d11a4c5faf652b83b85a9d879f3e6a7e8a3f9a1a
BrunoAnastacio <obrunoanastacio@gmail.com>
2024-01-13 14:45:35 -0300

    .'

--- src/main/java/org/example/guess/models/dao/Player.java
+++ src/main/java/org/example/guess/models/dao/Player.java
@@ -1,64 +1,3 @@
-<<<<<<<< HEAD:src/main/java/org/example/guess/models/beans/Player.java
-package org.example.guess.models.beans;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.sql.Timestamp;
-
-public class Player {
-    private String name;
-    //private int stars;
-    private int answers;
-    private Timestamp timestamp;
-
-    public Player(){
-        this.timestamp = new Timestamp(System.currentTimeMillis());
-    }
-
-    public Player(String name) {
-        this.name = name;
-        this.answers = 0;
-        this.timestamp = new Timestamp(System.currentTimeMillis());
-    }
-
-    public Player(String name, int answers){
-        this.name = name;
-        this.answers = answers;
-        this.timestamp = new Timestamp(System.currentTimeMillis());
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAnswers() {
-        return answers;
-    }
-
-    public void setAnswers() {
-        this.answers++;
-    }
-
-    public void setAnswers(int answers){
-        this.answers = answers;
-    }
-
-    @Override
-    public String toString() {
-        return "Player{" +
-                "name='" + name + '\'' +
-                ", answers=" + answers +
-                ", timestamp=" + timestamp +
-                '}';
-    }
-
-    public String toJson(){
-        Gson gson = new GsonBuilder().create();
-        return gson.toJson(this);
-    }
-}
-========
 package org.example.guess.models.dao;
 
 import com.google.gson.Gson;
@@ -118,4 +57,3 @@ public class Player {
         return gson.toJson(this);
     }
 }
->>>>>>>> 784474c (Iniciada implementação de PlayerBean):src/main/java/org/example/guess/models/dao/Player.java
