b66ca03b1eefbdeb2e617b8644677d74cf468c4d
BrunoAnastacio <obrunoanastacio@gmail.com>
2024-01-13 14:47:04 -0300

    resolvendo conflitos II

--- src/main/java/org/example/guess/models/bean/PlayerBean.java
+++ src/main/java/org/example/guess/models/bean/PlayerBean.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package org.example.guess.models.bean;
 
 import org.example.guess.connection.ConnectionFactory;
@@ -23,3 +24,35 @@ public class PlayerBean {
 
 
 }
+=======
+package org.example.guess.models.bean;
+
+import org.example.guess.connection.ConnectionFactory;
+import org.example.guess.models.dao.Player;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class PlayerBean {
+    public PlayerBean(String name, int answer) throws SQLException {
+        Player player = new Player(name, answer);
+        String sql = "INSERT INTO player VALUES(1, "+ player.getName()+","+ player.getAnswers()+")";
+        //Statement statement = connection.prepareStatement(sql);
+        Connection connection = new ConnectionFactory().recoveryConnection();
+
+        //statement.executeUpdate(sql);
+
+        //https://unibb.alura.com.br/course/java-jdbc-banco-dados/task/124473
+        //https://terminalroot.com.br/2022/06/como-conectar-ao-sqlite-com-java.html
+    }
+
+    //insert
+        //recoveryConnectionSQLite()
+        //statement.executeUpdate(sql)
+        //closeConnection()
+
+
+}
+>>>>>>> 65bc516 (desenvolvendo PlayerBean)
